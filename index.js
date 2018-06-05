0034fd06453eaf7fe8cfd253c43213bd3aee43d0
Niko Salminen <niko.salminen@nitro.fi>
2018-06-05 23:51:27 +0300

    Procfile added for heroku

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/info', (req, res) => {
     res.send(`Tietokannassa ${persons.length} nimeä`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
